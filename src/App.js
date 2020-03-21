860b65992675c7d4cebf599d3eb9fd7d6b4fc4e2
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-03-21 13:35:31 -0300

    Class components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 
+//import download from './assets/download.jpg';
+import TechList from './components/TechList';
+
 function App() {
-    return <h1>Hello Rocketseat</h1>
+    //return <h1>Hello Rocketseat</h1>
+    return <TechList />
 }
 
 export default App;
