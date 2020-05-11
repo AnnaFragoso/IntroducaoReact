72504172e490473aeed669edd0f26570580d09b5
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-05-10 21:04:21 -0300

    Default Props e PropTypes

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -33,7 +33,13 @@ class TechList extends Component {
             <>
                 <form onSubmit={this.handleSubmit}>
                 <ul>
-                    {this.state.techs.map(tech => <TechItem  key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} /> )}
+                    {this.state.techs.map(tech => 
+                        <TechItem  
+                            key={tech} 
+                            tech={tech} 
+                            onDelete={() => this.handleDelete(tech)} 
+                        /> )}
+                        <TechItem />
                 </ul>
                 <input 
                     type="text" 
